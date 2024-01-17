6dd12071ee2ff9a913b6bf69077ac7c03975a6fa
Luis <luismg26@yahoo.es>
2024-01-17 10:50:57 -0500

    Modificacion index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
+  <title>BIENVENIDO A OPENSHIFT</title>
 
 <?php include 'style.php' ?>
 
@@ -12,7 +12,7 @@
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift!</h1>
+            <h1>Aplicacion PHP echa en OPENSHIFT!</h1>
           </hgroup>
 
 <?php
